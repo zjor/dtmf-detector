84b8b4724f0e390326ad8e103dc3e0ab9bad0869
Sergey Royz <sergey.royz@goeuro.com>
2018-11-10 17:24:51 +0100

    removed redundant code

--- processing/src/main/java/com/github/zjor/App.java
+++ processing/src/main/java/com/github/zjor/App.java
@@ -1,73 +1,83 @@
 package com.github.zjor;
 
 import processing.core.PApplet;
+import processing.core.PFont;
 import processing.sound.*;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 
 public class App extends PApplet {
 
     public static final double SAMPLING_RATE = 44100.0;
-    public static final int BANDS = 2048;
+    public static final int BANDS = 256;
 
     private float bandWidth;
 
     private FFT fft;
     private float[] spectrum = new float[BANDS];
 
+    private PFont titleFont;
+    private PFont normalFont;
+
     @Override
     public void settings() {
-        size(800, 600);
-        bandWidth = width / BANDS;
+        size(600, 480);
+
     }
 
     @Override
     public void setup() {
+        String[] fontList = PFont.list();
+        printArray(fontList);
+
+        titleFont = createFont("Monospaced", 24);
+        normalFont = createFont("Monospaced", 16);
+
         background(255);
+
         AudioIn audioIn = new AudioIn(this);
         audioIn.start();
 
+        bandWidth = width / BANDS;
+
         fft = new FFT(this, BANDS);
         fft.input(audioIn);
-
     }
 
     @Override
     public void draw() {
         fft.analyze(spectrum);
-        List<Pair<Double, Double>> topFreqs = getTopFrequencies(spectrum);
 
         float[] s = Arrays.copyOf(spectrum, spectrum.length);
-        Pair<Integer, Double> firstFreq = indexedMax(s);
-        int from = limit((int) ((firstFreq.b - 35.f) * BANDS / SAMPLING_RATE * 2), 0, s.length - 1);
-        int to = limit((int) ((firstFreq.b + 35.f) * BANDS / SAMPLING_RATE * 2), 0, s.length - 1);
-//        System.out.println(from + "; " + to);
+        int firstFreqIndex = maxIndex(s);
+        double firstFreq = freq(firstFreqIndex);
+        int from = limit((int) ((firstFreq - 120.f) * BANDS / SAMPLING_RATE * 2), 0, s.length - 1);
+        int to = limit((int) ((firstFreq + 120.f) * BANDS / SAMPLING_RATE * 2), 0, s.length - 1);
 
         zero(s, from, to);
 
-        Pair<Integer, Double> secondFreq = indexedMax(s);
+        int secondFreqIndex = maxIndex(s);
+        double secondFreq = freq(secondFreqIndex);
 
 
         fill(255);
         rect(0, 0, width, height);
-
         fill(0);
+
+        textAlign(CENTER, TOP);
+        textFont(titleFont);
+        text("DTMF decoder", width / 2, 8, 0);
+
+
         for (int i = 0; i < BANDS; i++) {
             float h = spectrum[i] * height * 20;
             rect(i * bandWidth, height - h, bandWidth, h);
         }
 
-        for (int i = 0; i < 10; i++) {
-            text(String.format("%.2f", topFreqs.get(i).b), 16, 16 + i * 16, 0);
-        }
-
-        text(String.format("%.2f", firstFreq.b), 96, 16, 0);
-        text(String.format("%.2f", secondFreq.b), 96, 32, 0);
-        Character digit = detectDigit(firstFreq.b, secondFreq.b);
+        textFont(normalFont);
+        text(String.format("%.2f", firstFreq), 96, 16, 0);
+        text(String.format("%.2f", secondFreq), 96, 32, 0);
+        Character digit = resolveDigit(firstFreq, secondFreq);
         if (digit != null) {
             text(digit, 96, 96, 0);
         }
@@ -78,24 +88,15 @@ public class App extends PApplet {
         return SAMPLING_RATE * n / BANDS / 2;
     }
 
-    protected static List<Pair<Double, Double>> getTopFrequencies(float[] spectrum) {
-        List<Pair<Double, Double>> freqs = new ArrayList<>(spectrum.length);
 
-        for (int i = 0; i < spectrum.length; i++) {
-            freqs.add(Pair.of((double) spectrum[i], freq(i)));
-        }
-        Collections.sort(freqs, Comparator.comparing(a -> -a.a));
-        return freqs;
-    }
-
-    protected static Pair<Integer, Double> indexedMax(float[] a) {
+    protected static int maxIndex(float[] a) {
         int max = 0;
         for (int i = 0; i < a.length; i++) {
             if (a[max] < a[i]) {
                 max = i;
             }
         }
-        return Pair.of(max, freq(max));
+        return max;
     }
 
     protected static void zero(float[] a, int from, int to) {
@@ -126,8 +127,8 @@ public class App extends PApplet {
             {'*', '0', '#', 'D'},
     };
 
-    private static Character detectDigit(double f1, double f2) {
-        int column = matchFrequency(f1, COLUMN_FREQUENCIES, 50);
+    private static Character resolveDigit(double f1, double f2) {
+        int column = matchFrequency(f1, COLUMN_FREQUENCIES, 120);
         int row = matchFrequency(f2, ROW_FREQUENCIES, 35);
         if (column != -1 && row != -1) {
             return DTMF_TABLE[row][column];
@@ -138,26 +139,5 @@ public class App extends PApplet {
 
     public static void main(String[] args) {
         PApplet.main(App.class);
-    }
-}
-
-class Pair<A, B> {
-
-    public final A a;
-
-    public final B b;
-
-    public Pair(A a, B b) {
-        this.a = a;
-        this.b = b;
-    }
-
-    static <X, Y> Pair<X, Y> of(X a, Y b) {
-        return new Pair<>(a, b);
-    }
-
-    @Override
-    public String toString() {
-        return "Pair{" + "a=" + a + ", b=" + b + '}';
     }
 }
